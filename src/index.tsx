72185359a9cc9c086d13874856993153b0542746
”DavidKaySE” <david@asteo.se>
2024-10-10 15:55:13 +0200

    Posthog

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from '../src/App';
 import { Provider } from 'react-redux';
 import { store } from '../src/store/store';
-import App from '../src/App';
-import './index.css';
+import { PostHogProvider } from 'posthog-js/react'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PostHogProvider 
+        apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+        options={options}
+      >
+        <App />
+      </PostHogProvider>
     </Provider>
   </React.StrictMode>
 );
