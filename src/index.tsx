cb8a1146a5b4b8d6707e9c4971c3c945ad27fb1e
”DavidKaySE” <david@asteo.se>
2024-10-11 16:23:56 +0200

    posthog 3

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,19 @@ import { store } from '../src/store/store';
 import { PostHogProvider } from 'posthog-js/react'
 
 const options = {
-  api_host: 'https://webhook.site/098d6b4a-d18e-4270-9f78-dc100de099d8',
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PostHogProvider 
-        apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
-        options={options}
-      >
+    <PostHogProvider 
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <Provider store={store}>
         <App />
-      </PostHogProvider>
-    </Provider>
+      </Provider>
+    </PostHogProvider>
   </React.StrictMode>
 );
