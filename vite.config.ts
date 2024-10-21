3a9b5968e06eae99eacdf8c8b76114593a2dd521
”DavidKaySE” <david@asteo.se>
2024-10-21 10:13:15 +0200

    posthog

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,7 @@ export default defineConfig({
   define: {
     'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
     'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
+    'import.meta.env.VITE_POSTHOG_KEY': JSON.stringify(process.env.REACT_APP_PUBLIC_POSTHOG_KEY),
+    'import.meta.env.VITE_POSTHOG_HOST': JSON.stringify(process.env.REACT_APP_PUBLIC_POSTHOG_HOST),
   },
 });
